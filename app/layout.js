72cf30c44d35e91feaf206a1fbc2f33ce4307fc4
Speculate Dev <sida3li777@gmail.com>
2025-06-23 12:06:29 +0100

    Init: Font added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,13 @@ import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -15,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100">
+      <body className={`${josefin.className} bg-primary-950 text-primary-100`}>
         <header>
           <Logo />
           <Navigation />
