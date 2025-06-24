ec5264aa359a2bf4e661a5b31e55034d772058c0
Speculate Dev <sida3li777@gmail.com>
2025-06-24 07:36:13 +0100

    UI: account page layout

---
+++ app/account/layout.js
@@ -0,0 +1,12 @@
+import SideNavigation from "@/app/_components/SideNavigation";
+
+function Layout({ children }) {
+  return (
+    <div className="grid grid-cols-[auto_1fr] md:grid-cols-[16rem_1fr] md:gap-12 gap-4 h-full relative">
+      <SideNavigation />
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
--- app/account/page.js
+++ app/account/page.js
@@ -2,8 +2,14 @@ export const metadata = {
   title: "Guest area",
 };
 
-function page() {
-  return <div>Account</div>;
+async function page() {
+  // const session = await auth();
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {"firstname"}
+    </h2>
+  );
 }
 
 export default page;
