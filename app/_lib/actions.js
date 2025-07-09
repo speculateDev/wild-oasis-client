6353982e80e1f6ea7ae3525b4ad4cab86ce98790
Speculate Dev <sida3li777@gmail.com>
2025-07-09 23:16:23 +0100

    Action: SignIn with credentials

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,25 @@
 "use server";
 import { signIn, signOut } from "@/app/_lib/auth";
 
+export async function signInWithCredentials(credentials) {
+  const { email, password } = credentials;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: "/",
+    });
+  } catch (error) {
+    if (error.type === "CredentialsSignin") {
+      throw new Error(
+        "Invalid credentials. Please check your email and password."
+      );
+    } else {
+      throw new Error("An unexpected error occurred. Please try again later.");
+    }
+  }
+}
+
 export async function signInAction(provider) {
   await signIn(provider);
 }
