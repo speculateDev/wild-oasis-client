0cff1e364a34d5374053f8c9d99bc58ecaa598f6
Speculate Dev <sida3li777@gmail.com>
2025-07-09 13:14:53 +0100

    Next-Auth: init

---
+++ app/_lib/auth.js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+// import { getGuest } from "./data-service";
+
+export const authConfig = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+
+  callbacks: {},
+};
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth(authConfig);
