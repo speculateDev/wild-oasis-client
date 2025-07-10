e59b1be21b083011d95ebad965f9088b549a4dba
Speculate Dev <sida3li777@gmail.com>
2025-07-10 11:53:50 +0100

    Next-Auth: reset to the middleware redirection

--- middleware.js
+++ middleware.js
@@ -1,37 +1,35 @@
 import NextAuth from "next-auth";
-import { auth, authConfig } from "./app/_lib/auth";
+import { authConfig } from "./app/_lib/auth";
 import { NextResponse } from "next/server";
 import { protectedRoutes, authRoute } from "./routes";
 
-// const { auth } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
 
-// export default auth((req) => {
-//   const { nextUrl } = req;
-//   const isLoggedIn = !!req.auth;
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-//   const isProtected = protectedRoutes.some((route) =>
-//     nextUrl.pathname.startsWith(route)
-//   );
-//   const isAuthRoute = authRoute.includes(nextUrl.pathname);
+  const isProtected = protectedRoutes.some((route) =>
+    nextUrl.pathname.startsWith(route)
+  );
+  const isAuthRoute = authRoute.includes(nextUrl.pathname);
 
-//   if (!isLoggedIn && isProtected) {
-//     return NextResponse.redirect(new URL(`/login`, nextUrl.origin));
-//   }
+  if (!isLoggedIn && isProtected) {
+    return NextResponse.redirect(new URL(`/login`, nextUrl.origin));
+  }
 
-//   if (isAuthRoute && isLoggedIn) {
-//     return NextResponse.redirect(new URL(`/`, nextUrl.origin));
-//   }
+  if (isAuthRoute && isLoggedIn) {
+    return NextResponse.redirect(new URL(`/`, nextUrl.origin));
+  }
 
-//   return null;
-// });
-export const middleware = auth;
+  return null;
+});
 
 export const config = {
-  // matcher: [
-  //   // Skip Next.js internals and all static files, unless found in search params
-  //   "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-  //   // Always run for API routes
-  //   "/(api|trpc)(.*)",
-  // ],
-  matcher: ["/account"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
