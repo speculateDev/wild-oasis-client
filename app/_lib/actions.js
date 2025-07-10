10f9a356490bb0acf2bc9de8c52a9848c9a500f2
Speculate Dev <sida3li777@gmail.com>
2025-07-10 11:41:30 +0100

    Action: updateProfile

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,44 @@
 "use server";
-import { signIn, signOut } from "@/app/_lib/auth";
+import { auth, signIn, signOut } from "@/app/_lib/auth";
+import { supabase } from "./supabase";
+import { revalidatePath } from "next/cache";
+
+export async function updateProfile(formData) {
+  try {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+
+    // Log all form data
+    const nationalID = formData.get("nationalID");
+    const x = formData.get("nationality");
+    const [nationality, countryFlag] = x.split("%");
+
+    const nationalIDRegex = /^[a-zA-Z0-9]{6,12}$/;
+
+    if (!nationalIDRegex.test(nationalID)) {
+      throw new Error("Please provide a valid national ID");
+    }
+
+    const updateData = {
+      nationalID,
+      countryFlag,
+      nationality,
+    };
+
+    const { data, error } = await supabase
+      .from("guests")
+      .update(updateData)
+      .eq("id", session.user.id);
+
+    if (error) {
+      throw new Error("Guest could not be updated");
+    }
+
+    return { success: true };
+  } catch (error) {
+    return { error: error.message };
+  }
+}
 
 export async function signInWithCredentials(credentials) {
   const { email, password } = credentials;
