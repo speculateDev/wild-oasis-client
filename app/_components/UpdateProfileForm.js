94c419d0ab5cba58d9b1f76c6f7c4c808608fe9d
Speculate Dev <sida3li777@gmail.com>
2025-07-10 18:33:44 +0100

    Feat-profilePic-UI: image input

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,23 +1,62 @@
 "use client";
 
-import { ExclamationCircleIcon, FlagIcon } from "@heroicons/react/24/solid";
+import {
+  ExclamationCircleIcon,
+  FlagIcon,
+  ArrowUpTrayIcon,
+} from "@heroicons/react/24/solid";
 import SpinnerMini from "@/app/_components/SpinnerMini";
 import { useFormStatus } from "react-dom";
 import { updateProfile } from "../_lib/actions";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { uploadToCoudinary } from "../_lib/helpers";
 
 function UpdateProfileForm({ guest, children }) {
+  const imageInpRef = useRef(null);
+  const [img, setImg] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploaded, setUploaded] = useState(false);
+
   const { fullName, email, nationality, countryFlag, nationalID } = guest;
   const [error, setError] = useState(null);
 
   const handleSubmit = async (formData) => {
     setError(null);
 
-    const result = await updateProfile(formData);
-    if (result.error) {
-      setError(result.error);
+    try {
+      let imageUrl = null;
+      setIsUploading(true);
+
+      // Upload image if selected
+      if (img) {
+        const res = await uploadToCoudinary(img);
+        imageUrl = res.secure_url;
+
+        setUploaded(true);
+      }
+
+      // Append imgUrl if exists
+      if (imageUrl) {
+        formData.append("profile", imageUrl);
+      }
+
+      const result = await updateProfile(formData);
+
+      if (result.error) {
+        setError(result.error);
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsUploading(false);
     }
   };
+
+  function handleImgChange(e) {
+    const img = e.target.files[0];
+    setImg(img);
+    setUploaded(false);
+  }
 
   return (
     <form
@@ -64,6 +103,53 @@ function UpdateProfileForm({ guest, children }) {
         />
       </div>
 
+      <div className="mt-3">
+        <input
+          type="file"
+          ref={imageInpRef}
+          accept="image/*"
+          hidden
+          onChange={handleImgChange}
+        />
+        <div
+          onClick={() => imageInpRef.current.click()}
+          className="cursor-pointer p-4 rounded-lg border-2 border-dashed border-primary-600 text-center"
+        >
+          {uploaded ? (
+            <span className="text-accent-600 font-bold">Image Uploaded</span>
+          ) : (
+            <>
+              {img ? (
+                <p className="text-accent-400 font-bold">Image selected</p>
+              ) : (
+                <>
+                  <div className="justify-center flex">
+                    <div className="bg-accent-600 rounded-full p-4">
+                      <ArrowUpTrayIcon className="size-5" />
+                    </div>
+                  </div>
+
+                  <div className="mt-3">Upload Profile</div>
+                </>
+              )}
+              <button
+                type="button"
+                className="border border-accent-500 px-4 py-2 mt-2 focus:outline-none"
+              >
+                {isUploading ? (
+                  <span className="flex gap-3">
+                    <SpinnerMini />
+                    <span>Uploading image...</span>
+                  </span>
+                ) : (
+                  "Choose a file"
+                )}
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+
       {error && (
         <div className="text-red-500 text-center mb-4 font-bold tracking-wider">
           {
@@ -76,7 +162,7 @@ function UpdateProfileForm({ guest, children }) {
       )}
 
       <div className="flex justify-end items-center gap-6">
-        <Button pendingLabel="Updating...">Update Profile</Button>
+        <Button>Update Profile</Button>
       </div>
     </form>
   );
@@ -87,6 +173,7 @@ function Button() {
 
   return (
     <button
+      type="submit"
       disabled={pending}
       className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
     >
