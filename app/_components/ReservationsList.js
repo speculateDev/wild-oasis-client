7971ea308e26bad04fbc3abe66bbac2000b227e0
Speculate Dev <sida3li777@gmail.com>
2025-07-12 07:20:49 +0100

    UI: optimistic delete for instant update

--- app/_components/ReservationsList.js
+++ app/_components/ReservationsList.js
@@ -1,17 +1,25 @@
 "use client";
 
+import { useOptimistic } from "react";
 import { deleteBooking } from "../_lib/actions";
 import ReservationCard from "./ReservationCard";
 
 function ReservationsList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (currBookings, bookingId) => {
+      return currBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
   function handleDelete(bookingId) {
-    // optimisticDelete(bookingId);
+    optimisticDelete(bookingId);
     deleteBooking(bookingId);
   }
 
   return (
     <ul className="space-y-6">
-      {bookings.map((booking) => (
+      {optimisticBookings.map((booking) => (
         <ReservationCard
           key={booking.id}
           onDelete={handleDelete}
